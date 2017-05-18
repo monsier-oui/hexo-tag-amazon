ed979a6e5dbf7846c56ea7cceb2b1220786372e6
oui <hedgehog.oui@gmail.com>
2017-05-18 14:29:46 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# hexo-tag-amazon
+Plugin to generate a link to Amazon for HEXO. This depends on [apac](https://www.npmjs.com/package/apac).
+
+## Usage
+```
+{%amazon ISBN %}
+{%amazon ASIN %}
+```
+
+First, you should edit your `_config.yml` by adding following configuration.
+
+```yml
+amazon_plugin:
+  id: hogehoge-22 # Your Amazon associate ID
+  locale: US
+  accessKey: xxxxxxx # Your Amazon Product Advertising API Access key ID
+  accessKeySecret: xxxxxxxxxxxxxxx # Your Amazon Product Advertising API Secret access key
+```
+
+### locale
+See [here](https://www.npmjs.com/package/apac#locales).
+
+## license
+MIT
---
+++ index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var OperationHelper = require('apac').OperationHelper;
+
+hexo.extend.tag.register('amzn', function(args){
+  if(!args) return;
+  if(!hexo.config.amazon_plugin) return;
+
+  var asin = args[0];
+  var id = hexo.config.amazon_plugin.id;
+  if(!id) id = '87oui-22';
+  var locale = hexo.config.amazon_plugin.locale;
+  if(!locale) locale = 'US';
+
+  var opHelper = new OperationHelper({
+    awsId: hexo.config.amazon_plugin.accessKey,
+    awsSecret: hexo.config.amazon_plugin.accessKeySecret,
+    assocId: id,
+    locale: locale
+  });
+  return opHelper.execute('ItemLookup', {
+    'ItemId': asin,
+    'ResponseGroup': 'ItemAttributes,Images'
+  }).then(function(res){
+    var result = res.result.ItemLookupResponse;
+    if(result){
+      var item = result.Items.Item;
+      // console.log('Item: ', JSON.stringify(item));
+      var url = item.DetailPageURL;
+      var thumb = item.MediumImage.URL;
+
+      var attr = item.ItemAttributes;
+      var author = '';
+      switch (attr.ProductGroup){
+        case 'Book':
+          author = attr.Author;
+          break;
+        case 'DVD':
+          author = attr.Manufacturer;
+          break;
+        case 'Music':
+          author = attr.Artist;
+          break;
+        default:
+          author = attr.Brand;
+          break;
+      }
+      if(author.isArray){
+        author = author.join(', ');
+      }
+      var title = attr.Title;
+      var price = '';
+      if(attr.ListPrice) price = attr.ListPrice.FormattedPrice;
+      var releaseDate = '';
+      if(attr.ReleaseDate) releaseDate = attr.ReleaseDate;
+
+      var wishlist = item.ItemLinks.ItemLink[0];
+
+      var code = '<figure class="hexo-tag-amazon">'
+      +'<a href="'+url+'" class="hexo-tag-amazon-thumb"><img src="'+thumb+'"></a>'
+      +'<figcaption class="hexo-tag-amazon-caption">'
+      +'<a href="'+url+'" class="hexo-tag-amazon-title">'+title+'</a>'
+      +'<div class="hexo-tag-amazon-meta">';
+      if(author) code += '<span class="hexo-tag-amazon-author">'+author+'</span>';
+      if(releaseDate) code += '<span class="hexo-tag-amazon-releaseDate">'+releaseDate+'</span>';
+      if(price) code += '<span class="hexo-tag-amazon-price">'+price+'</span>';
+      code += '</div>'
+      +'<a href="'+url+'" class="hexo-tag-amazon-link">Buy</a>'
+      +'</figcaption>'
+      +'</figure>';
+
+      return code;
+    }else{
+      console.log(res.result.ItemLookupErrorResponse.Error.Message);
+    }
+  }).catch(function(err){
+    console.log('Something went wrong! ', err);
+  });
+},{
+  async: true
+});
